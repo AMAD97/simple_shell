a5c438e02e6caec885c088828b8855a9256ce299
AMAD97 <aminedaoui00@gmail.com>
2023-11-15 15:54:23 -0800

    update 4

--- shell.c
+++ shell.c
@@ -9,6 +9,8 @@
 extern char **environ;
 
 #define MAX_COMMAND_LENGTH 100
+#define MAX_ARGS 10
+
 
 void display_prompt(int is_piped) {
     if (!is_piped) {
@@ -19,9 +21,12 @@ void display_prompt(int is_piped) {
 
 void run_shell(const char *executable_name) {
     char command[MAX_COMMAND_LENGTH];
-
     while (1) {
         pid_t pid;
+    	char *args[MAX_ARGS + 1];
+    	int arg_count;
+	char *token;
+
 	int is_piped = isatty(STDIN_FILENO) == 0;
 	display_prompt(is_piped);
 
@@ -35,18 +40,31 @@ void run_shell(const char *executable_name) {
 
         command[strcspn(command, "\n")] = '\0';
 
+	token = strtok(command, " ");
+        arg_count = 0;
+	
+
+        while (token != NULL && arg_count < MAX_ARGS) {
+            args[arg_count++] = token;
+            token = strtok(NULL, " ");
+        }
+
+        while (token != NULL && arg_count < MAX_ARGS) {
+            args[arg_count++] = token;
+            token = strtok(NULL, " ");
+        }
+
+        args[arg_count] = NULL;
+
+
         pid = fork();
 
         if (pid == -1) {
             perror(executable_name);
             exit(EXIT_FAILURE);
         } else if (pid == 0) {
-            char *args[2];
-	    args[0] = command;
-	    args[1] = NULL;
-
-            if (execve(command, args, environ) == -1) {
-                perror(executable_name);
+            if (execvp(args[0], args) == -1) {
+                perror(args[0]);
                 _exit(EXIT_FAILURE);
             }
         } else {
